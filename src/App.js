adf4c69ee67c4ad23c6e325dc025719051008942
Navaneeth Devan <navaneethdevanofficial@gmail.com>
2022-05-31 19:25:22 +0530

    dumy static contents are added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import './App.css'
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+
+
 
 function App() {
   return (
     <div className="App">
-  
+     <NavBar/>
+     <Banner/>
+     <RowPost/>
     </div>
   );
 }
