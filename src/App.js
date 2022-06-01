91aae25541e3aa91ffaa69e5325c8bedc46124bf
Navaneeth Devan <navaneethdevanofficial@gmail.com>
2022-06-01 10:02:54 +0530

    some basic changes included and next set is the video preview section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
+import {action,originals,comedy,horror,romance} from './urls'
 import './App.css'
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
@@ -11,7 +12,12 @@ function App() {
     <div className="App">
      <NavBar/>
      <Banner/>
-     <RowPost/>
+     <RowPost url={originals}  title='Netflix Originals' />
+     <RowPost url={action}  title='Action' isSmall />
+     <RowPost url={comedy}  title='Comedy' isSmall />
+     <RowPost url={horror}  title='Horror' isSmall />
+     <RowPost url={romance} title='Romance' isSmall />
+    
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY='adf989a6de4ab9f4ac485e6b69a47c31'; 
+export const imageUrl="https://image.tmdb.org/t/p/original";
